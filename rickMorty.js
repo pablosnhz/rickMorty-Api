057a433a004cd1a080bd60d7d7d0be11182eabe6
PabloSnhz <pablosanhuezaaa@gmail.com>
2023-11-18 14:56:52 -0300

    estilo

--- rickMorty.js
+++ rickMorty.js
@@ -1,5 +1,17 @@
+let characters = document.querySelector('#characters');
+
 fetch('https://rickandmortyapi.com/api/character')
     .then( ( resp ) => resp.json() )
     .then( ( data ) => {
-        console.log(data)
-    } )
+        console.log(data.results)
+    
+        data.results.map( (item) => {
+            const content = document.createElement('div')
+            content.innerHTML = 
+            `
+            <img src="${item.image}">
+            <h2>${item.name}</h2>
+            `;
+            characters.append(content)
+        } )
+    });
